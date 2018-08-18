945eff165c0de153e5fbaade5084a9d5d0c50897
tfogg2 <foggts@g.cofc.edu>
2018-08-18 13:41:32 -0400

    photo credit

--- camuro/src/Components/Gallery.js
+++ camuro/src/Components/Gallery.js
@@ -68,18 +68,21 @@ class Gallery extends Component {
 }
 
 const GALLERY = [
-  { image: require("../Assets/chad-dalke-mini.jpg"), title: "Chad Dalke", credit: "https://unsplash.com/photos/4fydl6bJT8k?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"},
-  { image: require("../Assets/filtergrade-nikon-mini.jpg"), title: "Filter Grade", credit: "https://unsplash.com/photos/vn5MU5VwdjQ?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"},
-  { image: require("../Assets/joseph-chan-mini.jpg"), title: "Joseph Chan", credit: "https://unsplash.com/photos/99Ty_Vme3wA?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"},
-  { image: require("../Assets/jesus-santos-mini.jpg"), title: "Jesus Santos", credit: "https://unsplash.com/photos/_T3upUiCqEE?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"},
-  { image: require("../Assets/stephen-cook-mini.jpg"), title: "Stephen Cook", credit: "https://unsplash.com/photos/g1iJMOu0sIg?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"},
-  { image: require("../Assets/sebastian-pichler-mini.jpg"), title: "Sebastian Pichler", credit: "https://unsplash.com/photos/MDGpwpMY2Ws?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"},
-  { image: require("../Assets/chuttersnap.jpg"), title: "Sebastian Pichler", credit: ""},
-  { image: require("../Assets/jeff-sheldon.jpg"), title: "Jeff Sheldon", credit: ""},
-  { image: require("../Assets/marc-mueller.jpg"), title: "Marc Mueller", credit: ""},
-  { image: require("../Assets/robert-shunev.jpg"), title: "Robert Shunev", credit: ""},
-  { image: require("../Assets/sonnie-hiles.jpg"), title: "Sonnie Hiles", credit: ""},
-  { image: require("../Assets/ehimetalor-unuabona.jpg"), title: "Ehimetalor Unuabona", credit: ""},
+  { image: require("../Assets/chad-dalke-mini.jpg"), title: "Chad Dalke", credit: "https://unsplash.com/@c3d"},
+  { image: require("../Assets/filtergrade-nikon-mini.jpg"), title: "Filter Grade", credit: "https://unsplash.com/@filtergrade"},
+  { image: require("../Assets/joseph-chan-mini.jpg"), title: "Joseph Chan", credit: "https://unsplash.com/@yulokchan"},
+  { image: require("../Assets/jesus-santos-mini.jpg"), title: "Jesus Santos", credit: "https://unsplash.com/@eflash35"},
+  { image: require("../Assets/stephen-cook-mini.jpg"), title: "Stephen Cook", credit: "https://unsplash.com/@3rdspace_photo"},
+  { image: require("../Assets/sebastian-pichler-mini.jpg"), title: "Sebastian Pichler", credit: "https://unsplash.com/@pichler_sebastian"},
+  { image: require("../Assets/chuttersnap.jpg"), title: "Chuttersnap", credit: "https://unsplash.com/@chuttersnap"},
+  { image: require("../Assets/jeff-sheldon.jpg"), title: "Jeff Sheldon", credit: "https://unsplash.com/@ugmonk"},
+  { image: require("../Assets/marc-mueller.jpg"), title: "Marc Mueller", credit: "https://unsplash.com/@seven11nash"},
+  { image: require("../Assets/robert-shunev.jpg"), title: "Robert Shunev", credit: "https://unsplash.com/@rshunev"},
+  { image: require("../Assets/sonnie-hiles.jpg"), title: "Sonnie Hiles", credit: "https://unsplash.com/@sonniehiles"},
+  { image: require("../Assets/ehimetalor-unuabona.jpg"), title: "Ehimetalor Unuabona", credit: "https://unsplash.com/@theeastlondonphotographer"},
+  { image: require("../Assets/baher-khairy.jpg"), title: "Baher Khairy", credit: "https://unsplash.com/@baher366"},
+  { image: require("../Assets/chris-lawton.jpg"), title: "Chris Lawton", credit: "https://unsplash.com/@chrislawton"},
+  { image: require("../Assets/jakob-owens.jpg"), title: "Jakob Owens", credit: "https://unsplash.com/@jakobowens1"},
 ]
 
 export default Gallery;
