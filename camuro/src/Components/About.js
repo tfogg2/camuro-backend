4c7a5037d02bc66cbad3e395abea4dbd656d74f7
tfogg2 <foggts@g.cofc.edu>
2018-08-27 10:49:21 -0400

    about wording

--- camuro/src/Components/About.js
+++ camuro/src/Components/About.js
@@ -4,11 +4,11 @@ import scrollToComponent from 'react-scroll-to-component'
 
 class About extends Component {
   render(){
-    return(  
+    return(
       <div className="about">
         <div className="about-copy">
           <img src={require('../Assets/mission3.svg')} alt="mission" />
-          <p>Our mission here at camuro.co is to help keep the film community alive, one restoration at a time. Interested in giving back while selling some unwanted gear? <span onClick={() => scrollToComponent(this.props.offer, { offset: 0, align: 'top', duration: 1500})}>Fill out the form below</span> and we'll get back to you with an offer.</p>
+          <p>Our mission here at camuro.co is to keep the film community alive, one restoration at a time. Interested in selling some unwanted gear while giving back to the film community? <span onClick={() => scrollToComponent(this.props.offer, { offset: 0, align: 'top', duration: 1500})}>Fill out the form below</span> and we'll get back to you with an offer.</p>
         </div>
       </div>
     )
