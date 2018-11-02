8c1c14724818776d3e927f942b4514c1e3577cad
tfogg2 <foggts@g.cofc.edu>
2018-11-02 18:05:53 -0400

    secret var

--- constants/stripe.js
+++ constants/stripe.js
@@ -1,8 +1,8 @@
 const configureStripe = require('stripe');
 
 const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production'
-    ? { process.env.SECRET_KEY }
-    : { process.env.SECRET_KEY };
+    ?  process.env.SECRET_KEY
+    :  process.env.SECRET_KEY ;
 
 const stripe = configureStripe(STRIPE_SECRET_KEY);
 
