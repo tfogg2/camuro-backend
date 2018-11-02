889536364aaf870cf88aebb1432c86d90501c16b
tfogg2 <foggts@g.cofc.edu>
2018-11-02 13:03:25 -0400

    secure keys

--- constants/stripe.js
+++ constants/stripe.js
@@ -2,7 +2,7 @@ const configureStripe = require('stripe');
 
 const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production'
     ? 'sk_live_MY_SECRET_KEY'
-    : 'sk_test_BLSyxB70gGuwTP3dc0hevORZ';
+    : 'sk_test_BLSyxB70gGuwTP3dc0hevOR';
 
 const stripe = configureStripe(STRIPE_SECRET_KEY);
 
