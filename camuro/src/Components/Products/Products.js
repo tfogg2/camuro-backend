29f8b1fd271e3b452747a6e94c20a9ce564f6f28
tfogg2 <foggts@g.cofc.edu>
2018-11-06 17:11:38 -0500

    product update

--- camuro/src/Components/Products/Products.js
+++ camuro/src/Components/Products/Products.js
@@ -88,6 +88,32 @@ class Products extends Component {
 // data
 const PRODUCTS = [
 
+  { image: require('../../Assets/Products/canonF1-1-featured.png'),
+    images: [
+      {original: require('../../Assets/Products/canonF1-1.png'), thumbnail: require('../../Assets/Products/canonF1-1-small.png')},
+      {original: require('../../Assets/Products/canonF1-2.png'), thumbnail: require('../../Assets/Products/canonF1-2-small.png')},
+      {original: require('../../Assets/Products/canonF1-3.png'), thumbnail: require('../../Assets/Products/canonF1-3-small.png')},
+      {original: require('../../Assets/Products/canonF1-4.png'), thumbnail: require('../../Assets/Products/canonF1-4-small.png')},
+      {original: require('../../Assets/Products/canonF1-5.png'), thumbnail: require('../../Assets/Products/canonF1-5-small.png')},
+      {original: require('../../Assets/Products/canonF1-6.png'), thumbnail: require('../../Assets/Products/canonF1-6-small.png')},
+      {original: require('../../Assets/Products/canonF1-7.png'), thumbnail: require('../../Assets/Products/canonF1-7-small.png')},
+      {original: require('../../Assets/Products/canonF1-8.png'), thumbnail: require('../../Assets/Products/canonF1-8-small.png')},
+      {original: require('../../Assets/Products/canonF1-9.png'), thumbnail: require('../../Assets/Products/canonF1-9-small.png')},
+      {original: require('../../Assets/Products/canonF1-10.png'), thumbnail: require('../../Assets/Products/canonF1-10-small.png')},
+      {original: require('../../Assets/Products/canonF1-11.png'), thumbnail: require('../../Assets/Products/canonF1-11-small.png')},
+      {original: require('../../Assets/Products/canonF1-12.png'), thumbnail: require('../../Assets/Products/canonF1-12-small.png')},
+      {original: require('../../Assets/Products/canonF1-13.png'), thumbnail: require('../../Assets/Products/canonF1-13-small.png')},
+      {original: require('../../Assets/Products/canonF1-14.png'), thumbnail: require('../../Assets/Products/canonF1-14-small.png')}
+    ],
+    title: "Canon F1, 28mm w NiCd Pack",
+    description: "Cosmetically in beautiful condition. We do our best to show the cameras in a strong and revealing light. Please zoom and inspect the hi-res images and feel free to ask us any questions before purchase. We don’t sell junkers. Ever.",
+    mechanics: "Focus: Smooth throughout \n Diaphragm: Clean & dry with snappy action-no oil \n Shutter: Speeds accurate throughout the range \n Film Transport: Clean and accurate. 36 frames possible. Film advance in strong and smooth throughout. \n Light Seals: New \n Meter: Functional and accurate \n Viewfinder: Big, clean and bright-minor micro dust-no affect \n",
+    optics:"Clean, clear and bright \n No fungus, haze, scratches, separation or pitting  \n Coatings are clean and bright on front & rear \n Minimal internal micro dust seen under intense light-no impact on image quality.",
+    category: "SLRs",
+    price: 449,
+    sold: false
+  },
+
   { image: require('../../Assets/Products/canonFd50-1-featured.png'),
     images: [
       {original: require('../../Assets/Products/canonFd50-1.png'), thumbnail: require('../../Assets/Products/canonFd50-1-small.png')},
@@ -103,9 +129,9 @@ const PRODUCTS = [
       {original: require('../../Assets/Products/canonFd50-11.png'), thumbnail: require('../../Assets/Products/canonFd50-11-small.png')},
     ],
     title: "Canon nF1 w nFD 50mm 1.8",
-    description: "",
-    mechanics: "",
-    optics:"",
+    description: "Cosmetically in beautiful condition. We do our best to show the cameras in a strong and revealing light. Please zoom and inspect the hi-res images and feel free to ask us any questions before purchase. We don’t sell junkers. Ever.",
+    mechanics: "Focus: Smooth throughout \n Diaphragm: Clean & dry with snappy action-no oil \n Shutter: Speeds accurate throughout the range \n Film Transport: Clean and accurate. 36 frames possible. Film advance in strong and smooth throughout. \n Light Seals: New \n Meter: Functional and accurate \n Viewfinder: Big, clean and bright-minor micro dust-no affect \n",
+    optics:"Clean, clear and bright \n No fungus, haze, scratches, separation or pitting  \n Coatings are clean and bright on front & rear \n Minimal internal micro dust seen under intense light-no impact on image quality.",
     category: "SLRs",
     price: 359,
     sold: false
@@ -130,9 +156,9 @@ const PRODUCTS = [
       {original: require('../../Assets/Products/konicaHex-15.png'), thumbnail: require('../../Assets/Products/konicaHex-15-small.png')},
     ],
     title: "Konica Hexar RF w 50mm M-Hexanon",
-    description: "",
-    mechanics: "",
-    optics:"",
+    description: "Cosmetically in beautiful condition. We do our best to show the cameras in a strong and revealing light. Please zoom and inspect the hi-res images and feel free to ask us any questions before purchase. We don’t sell junkers. Ever. \n This 50mm f2 Hexar focus scale and nameplate has been professionally refinished in a white and matte black  “Stealth Edition” to match the Camera paint and it looks flawless. Great for when you don’t want to be too flashy but still want those shots. A unique pairing, ready for the streets.",
+    mechanics: "Focus: Smooth throughout  \n Diaphragm: Clean & dry-no oil  \n Shutter: Speeds accurate throughout the range B-1/4000  \n Film Transport: Clean and strong. 36+ frames possible. Film advance in strong and smooth throughout.  \n Light Seals: New  \n Meter: Functional and accurate  \n Viewfinder: Big, clean and bright.  \n Rangefinder: Contrasty and easy to nail focus. Better than any Leica VF this side of an M3. A joy to use",
+    optics:"Mint \n Clean, clear and bright \n No fungus, haze, scratches, separation or pitting  \n Coatings are clean and bright on front & rear \n Optically its as good as it gets",
     category: "Rangefinders",
     price: 1499,
     sold: false
@@ -149,11 +175,31 @@ const PRODUCTS = [
       {original: require('../../Assets/Products/leicaRed-7.png'), thumbnail: require('../../Assets/Products/leicaRed-7-small.png')},
     ],
     title: "Leica iiif Red Dial",
-    description: "",
-    mechanics: "",
+    description: "Cosmetically in beautiful condition. We do our best to show the cameras in a strong and revealing light. Please zoom and inspect the hi-res images and feel free to ask us any questions before purchase. We don’t sell junkers. Ever.",
+    mechanics: "Shutter: Speeds accurate throughout the range  \n Film Transport: Clean and strong. 40 frames possible. Film advance in strong and smooth throughout. \n Light Seals: Light tight  \n Meter: No meter! \n Viewfinder: Big, clean and bright. \n Rangefinder: Contrasty and easy to nail focus",
     optics:"",
     category: "Rangefinders",
     price: 329,
+    sold: false
+  },
+
+  { image: require('../../Assets/Products/leicaiiib-1-featured.png'),
+    images: [
+      {original: require('../../Assets/Products/leicaiiib-1.png'), thumbnail: require('../../Assets/Products/leicaiiib-1-small.png')},
+      {original: require('../../Assets/Products/leicaiiib-2.png'), thumbnail: require('../../Assets/Products/leicaiiib-2-small.png')},
+      {original: require('../../Assets/Products/leicaiiib-3.png'), thumbnail: require('../../Assets/Products/leicaiiib-3-small.png')},
+      {original: require('../../Assets/Products/leicaiiib-4.png'), thumbnail: require('../../Assets/Products/leicaiiib-4-small.png')},
+      {original: require('../../Assets/Products/leicaiiib-5.png'), thumbnail: require('../../Assets/Products/leicaiiib-5-small.png')},
+      {original: require('../../Assets/Products/leicaiiib-6.png'), thumbnail: require('../../Assets/Products/leicaiiib-6-small.png')},
+      {original: require('../../Assets/Products/leicaiiib-7.png'), thumbnail: require('../../Assets/Products/leicaiiib-7-small.png')},
+      {original: require('../../Assets/Products/leicaiiib-8.png'), thumbnail: require('../../Assets/Products/leicaiiib-8-small.png')}
+    ],
+    title: 'Leica iiib Rangefinder',
+    description: "Cosmetically in excellent condition. We do our best to show the cameras in a strong and revealing light. Signs of use in the form of some “bright marks”. Please zoom and inspect the hi-res images and feel free to ask us any questions before purchase. We don’t sell junkers. Ever. \n This particular iiib comes with an “of era” modification for flash sync! A unique and rare edition to an already hard to find rangefinder.",
+    mechanics:"Shutter: Speeds accurate throughout the range  \n Film Transport: Clean and strong. 40 frames possible. Film advance in strong and smooth throughout. \n Light Seals: Light tight  \n Meter: No meter! \n Viewfinder: Big, clean and bright. \n Rangefinder: Contrasty and easy to nail focus",
+    optics:"",
+    category: "Rangefinders",
+    price: 339,
     sold: false
   },
 
@@ -175,9 +221,9 @@ const PRODUCTS = [
       {original: require('../../Assets/Products/mamiya330-14.png'), thumbnail: require('../../Assets/Products/mamiya330-14-small.png')},
     ],
     title: "Mamiya C330 Pro S Blue Dot 80mm 2.8",
-    description: "",
-    mechanics: "",
-    optics:"",
+    description: "Cosmetically in beautiful condition. We do our best to show the cameras in a strong and revealing light. Please zoom and inspect the hi-res images and feel free to ask us any questions before purchase. We don’t sell junkers. Ever.",
+    mechanics: "Focus: Bellows. Smooth throughout  \n Diaphragm: Clean & dry-no oil  \n Shutter: Speeds accurate throughout the range  \n Film Transport: Clean and strong. Film advance in strong and smooth throughout. \n Light Seals: New \n Meter: No meter! \n Viewfinder: Big, clean and bright. Easy to nail focus",
+    optics:"Mint \n Clean, clear and bright \n No fungus, haze, scratches, separation or pitting  \n Coatings are clean and bright on front & rear \n Optically its as good as it gets",
     category: "TLRs",
     price: 999,
     sold: false
@@ -198,9 +244,9 @@ const PRODUCTS = [
       {original: require('../../Assets/Products/minolta7-11.png'), thumbnail: require('../../Assets/Products/minolta7-11-small.png')}
     ],
     title: "Minolta 7sii Rangefinder",
-    description: "",
-    mechanics: "",
-    optics:"",
+    description: "Cosmetically in beautiful condition. We do our best to show the cameras in a strong and revealing light. Please zoom and inspect the hi-res images and feel free to ask us any questions before purchase. We don’t sell junkers. Ever.",
+    mechanics: "Focus: Smooth throughout  \n Diaphragm: Clean & dry-no oil  \n Shutter: Speeds accurate throughout the range  \n Film Transport: Clean and strong. 36+ frames possible. Film advance in strong and smooth throughout. \n Light Seals: New \n Meter: Functional and accurate  \n Viewfinder: Big, clean and bright. \n Rangefinder: Contrasty and easy to nail focus",
+    optics:"Mint \n Clean, clear and bright \n No fungus, haze, scratches, separation or pitting  \n Coatings are clean and bright on front & rear \n Optically its as good as it gets",
     category: "Rangefinders",
     price: 309,
     sold: false
@@ -219,9 +265,9 @@ const PRODUCTS = [
       {original: require('../../Assets/Products/minoltaMd-9.png'), thumbnail: require('../../Assets/Products/minoltaMd-9-small.png')}
     ],
     title: "Minolta MD Rokkor-X 35mm f1.8",
-    description: "",
+    description: "Cosmetically in beautiful condition. We do our best to show the cameras in a strong and revealing light. Please zoom and inspect the hi-res images and feel free to ask us any questions before purchase. We don’t sell junkers. Ever.",
     mechanics: "",
-    optics:"",
+    optics:"Excellent \n Clean, clear and bright \n One minor, 1mm coating scratch on front element as pictured \n Besides that, no fungus, haze, scratches, separation or pitting  \n Coatings are clean and bright on front & rear \n Optically it performs flawlessly. No image impact from the coating mark.",
     category: "Lenses",
     price: 229,
     sold: false
@@ -239,9 +285,9 @@ const PRODUCTS = [
       {original: require('../../Assets/Products/nikkorH-8.png'), thumbnail: require('../../Assets/Products/nikkorH-8-small.png')}
     ],
     title: "Nikkor-H 28mm f3.5 non-Ai w HS-10 Hood",
-    description: "",
+    description: "Cosmetically in beautiful condition. We do our best to show the cameras in a strong and revealing light. Please zoom and inspect the hi-res images and feel free to ask us any questions before purchase. We don’t sell junkers. Ever.",
     mechanics: "",
-    optics:"",
+    optics:"Excellent \n Clean, clear and bright \n No fungus, haze, scratches, separation or pitting  \n Coatings are clean and bright on front & rear \n Optically it performs flawlessly. No image impact from the coating mark.",
     category: "Lenses",
     price: 99,
     sold: false
@@ -389,25 +435,6 @@ const PRODUCTS = [
     sold: true
   },
 
-  { image: require('../../Assets/Products/leicaiiib-1-featured.png'),
-    images: [
-      {original: require('../../Assets/Products/leicaiiib-1.png'), thumbnail: require('../../Assets/Products/leicaiiib-1-small.png')},
-      {original: require('../../Assets/Products/leicaiiib-2.png'), thumbnail: require('../../Assets/Products/leicaiiib-2-small.png')},
-      {original: require('../../Assets/Products/leicaiiib-3.png'), thumbnail: require('../../Assets/Products/leicaiiib-3-small.png')},
-      {original: require('../../Assets/Products/leicaiiib-4.png'), thumbnail: require('../../Assets/Products/leicaiiib-4-small.png')},
-      {original: require('../../Assets/Products/leicaiiib-5.png'), thumbnail: require('../../Assets/Products/leicaiiib-5-small.png')},
-      {original: require('../../Assets/Products/leicaiiib-6.png'), thumbnail: require('../../Assets/Products/leicaiiib-6-small.png')},
-      {original: require('../../Assets/Products/leicaiiib-7.png'), thumbnail: require('../../Assets/Products/leicaiiib-7-small.png')},
-      {original: require('../../Assets/Products/leicaiiib-8.png'), thumbnail: require('../../Assets/Products/leicaiiib-8-small.png')}
-    ],
-    title: 'Leica iiib Rangefinder, “Of Era, A.C. Muller” Modified Flash Synchro w/ Silver Body Cap',
-    description: "",
-    mechanics:"",
-    optics:"",
-    category: "Rangefinders",
-    price: 339,
-    sold: true
-  },
   { image: require('../../Assets/Products/leicaSummi40-1-featured.png'),
     images: [],
     title: "Leica Summicron 40mm f2",
